ca51ab98b9d0d4ae17be45dfbd1ebe8049299315
SerchAndrew <sergiogarcia.anva@gmail.com>
2018-05-17 11:00:16 -0500

    cambiosEstaciones1

--- application/models/DbTable/DetalleProcesos.php
+++ application/models/DbTable/DetalleProcesos.php
@@ -17,7 +17,8 @@ class Application_Model_DbTable_DetalleProcesos extends Zend_Db_Table_Abstract
 			"nombre_operador"=>$copiardecaptura[0]["nombre_operador"],
 			"cant_requerida"=>$copiardecaptura[0]["cant_requerida"],
 			"fechahora_registro"=>$copiardecaptura[0]["fechahora_registro"],
-			"fechahora_inicio"=>$copiardecaptura[0]["fechahora_inicio"]
+			"fechahora_inicio"=>$copiardecaptura[0]["fechahora_inicio"],
+			"situacion"=>$copiardecaptura[0]["situacion"]
 			));
 
         $db = Zend_Db_Table_Abstract::getDefaultAdapter();
@@ -33,7 +34,7 @@ class Application_Model_DbTable_DetalleProcesos extends Zend_Db_Table_Abstract
 		return $response;
 	}	
 
-	public function reportarproceso($id_detalle,$idmaquina,$maquina,$idproceso,$proceso,$cantidadok,$cantidadmerma,$tiempo,$notas,$parcial)
+	public function reportarproceso($id_detalle,$idmaquina,$maquina,$idproceso,$proceso,$cantidadok,$cantidadmerma,$tiempo,$notas,$parcial,$situacion)
 	{
 		$where[] = "id = '$id_detalle'";
 		$update = $this->update(array(
@@ -45,7 +46,8 @@ class Application_Model_DbTable_DetalleProcesos extends Zend_Db_Table_Abstract
 			"cantidad_merma"=>$cantidadmerma,
 			"tiempo"=>$tiempo,
 			"notas"=>$notas,
-			"parcial"=>$parcial
+			"parcial"=>$parcial,
+			"situacion"=>$situacion
 			), $where);
 
 		$response = new stdClass();
@@ -53,7 +55,7 @@ class Application_Model_DbTable_DetalleProcesos extends Zend_Db_Table_Abstract
 		return $response;
 	}
 
-	public function reportarajuste($id_detalle,$idmaquina,$maquina,$idproceso,$proceso,$cantidadok,$cantidadmerma,$tiempo,$notas,$parcial)
+	public function reportarajuste($id_detalle,$idmaquina,$maquina,$idproceso,$proceso,$cantidadok,$cantidadmerma,$tiempo,$notas,$parcial,$situacion)
 	{
 		$where[] = "id = '$id_detalle'";
 		$update = $this->update(array(			
@@ -65,7 +67,8 @@ class Application_Model_DbTable_DetalleProcesos extends Zend_Db_Table_Abstract
 			"cantidad_merma"=>$cantidadmerma,
 			"tiempo"=>$tiempo,
 			"notas"=>$notas,
-			"parcial"=>$parcial
+			"parcial"=>$parcial,
+			"situacion"=>$situacion
 			), $where);
 
 		$response = new stdClass();
@@ -95,7 +98,8 @@ class Application_Model_DbTable_DetalleProcesos extends Zend_Db_Table_Abstract
        "cant_requerida"=>$row["cant_requerida"],
 		"id_operador" => $row["id_operador"],
 		"nombre_operador" => $row["nombre_operador"],       
-       "fechahora_registro"=>$row["fechahora_registro"]       
+       "fechahora_registro"=>$row["fechahora_registro"],
+       "situacion"=>$row["situacion"]       
      );
    }
    return $copiar;
@@ -121,7 +125,8 @@ class Application_Model_DbTable_DetalleProcesos extends Zend_Db_Table_Abstract
 				"id_proceso"=>$row["id_proceso"],
 				"nombre_proceso"=>$row["nombre_proceso"],
 				"cant_requerida"=>$row["cant_requerida"],
-				"fechahora_registro"=>$row["fechahora_registro"]
+				"fechahora_registro"=>$row["fechahora_registro"],
+				"situacion"=>$row["situacion"]
 				); 
 		}
 
